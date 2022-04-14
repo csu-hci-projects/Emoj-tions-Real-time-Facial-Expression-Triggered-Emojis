033e3789b037f3a5d6129c99e79b4a4ab08d4be6
Jake Mismas <55728241+jakemismas@users.noreply.github.com>
2022-04-14 16:58:05 -0600

    Initial Commit

---
+++ Shared/EmojtionApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojtionApp.swift
+//  Shared
+//
+//  Created by Jake Mismas on 4/14/22.
+//
+
+import SwiftUI
+
+@main
+struct EmojtionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
